e4be0d43f4c0d6248e0176fafb3ecaa398e0b01c
Will Bishop <17292672+WillBishop@users.noreply.github.com>
2022-05-17 12:58:08 +0930

    Smooth scrolling

--- ClassRoom Attendance/StudentCollectionViewCell.swift
+++ ClassRoom Attendance/StudentCollectionViewCell.swift
@@ -13,9 +13,47 @@ class StudentCollectionViewCell: UICollectionViewCell {
     @IBOutlet var studentRoll: UILabel!
     func setup(with student: Student)
     {
-        let image = UIImage(data:student.image! as Data)
-        studentImage.image = image
+		let imageData = student.image
+		let maxWidth = Int(self.bounds.width)
+		DispatchQueue(label: "com.imagedecode", qos: .userInteractive, attributes: .concurrent).async { [weak self] in
+			guard let self = self else { return }
+			if let imageData = imageData, let image = imageData.scaleImageData(toMaximumPixelCount: maxWidth) {
+				DispatchQueue.main.async {
+					self.studentImage.image = image
+				}
+			}
+		}
         studentName.text = student.name
         studentRoll.text = String(student.roll)
     }
+}
+
+extension Data {
+	func scaleImageData(toMaximumPixelCount: Int) -> UIImage? {
+		let imageSourceOptions = [kCGImageSourceShouldCache: false] as CFDictionary
+		if let source = CGImageSourceCreateWithData(self as CFData, imageSourceOptions) {
+			let downsampleOptions = [
+				kCGImageSourceCreateThumbnailFromImageAlways: true,
+				kCGImageSourceShouldCacheImmediately: true,
+				kCGImageSourceCreateThumbnailWithTransform: true,
+				kCGImageSourceThumbnailMaxPixelSize: 768
+			] as CFDictionary
+			
+			if let downsampleImage = CGImageSourceCreateThumbnailAtIndex(source, 0, downsampleOptions) {
+				let downsampled = UIImage(cgImage: downsampleImage)
+				return downsampled
+			}
+		}
+		return nil
+	}
+	enum ImageDataError: Error {
+		case invalidImageData
+	}
+	func throwingScale(toMaximumPixelCount: Int) throws -> UIImage {
+		if let image = self.scaleImageData(toMaximumPixelCount: toMaximumPixelCount) {
+			return image
+		} else {
+			throw ImageDataError.invalidImageData
+		}
+	}
 }
